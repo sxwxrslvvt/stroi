30dd4e94b553e1244410603e8bffa1c52d0f0bc3
sxwxrslvvt <sxwxrslvvt@gmail.com>
2025-03-10 13:45:38 +0300

    test

---
+++ index.php
@@ -0,0 +1,16 @@
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetPageProperty("description", "О компании \"Строительная техника\". Оригинальные и аналоговые запчасти импортного и отечественного производства.");
+$APPLICATION->SetPageProperty("title", "О поставщике запчастей, навесного оборудования и расходных материалов | Строительная техника");
+$APPLICATION->SetTitle("О компании");
+?>
+<link rel="stylesheet" href="style.css">
+
+<!-- Твоя верстка тут -->
+
+<div class="container">
+	баннер
+</div>
+
+<!-- Твоя верстка тут -->
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
